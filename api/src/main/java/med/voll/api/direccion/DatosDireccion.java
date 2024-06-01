d09bfc72d515cf6b7ea3b62b7def30b7288db6cf
Fair Lucas <lukitaa.27@gmail.com>
2024-06-01 12:26:38 -0300

    commit 2

--- api/src/main/java/med/voll/api/direccion/DatosDireccion.java
+++ api/src/main/java/med/voll/api/direccion/DatosDireccion.java
@@ -1,6 +1,17 @@
 package med.voll.api.direccion;
 
+import jakarta.validation.constraints.NotBlank;
 
-public record DatosDireccion(String calle, String distrito, String ciudad, String numero, String complemento) {
 
+public record DatosDireccion(
+        @NotBlank
+        String calle,
+        @NotBlank
+        String distrito,
+        @NotBlank
+        String ciudad,
+        @NotBlank
+        String numero,
+        @NotBlank
+        String complemento) {
 }
